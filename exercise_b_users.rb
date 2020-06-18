644e48f27d8598562bf318e8c02aa5ddc8cddcb7
Rebeka Geczi <geczirebeka@gmail.com>
2020-06-18 10:22:49 +0100

    fourteenth commit

--- exercise_b_users.rb
+++ exercise_b_users.rb
@@ -61,10 +61,39 @@ p users ["Erik"][:home_town]
 # 3. Get the array of Erik's lottery numbers
 p users ["Erik"][:lottery_numbers]
 # 4. Get the species of Avril's pet Monty
+p users ["Avril"][:pets][0][:species]
 # 5. Get the smallest of Erik's lottery numbers
-p users ["Erik"][:lottery_numbers][2]
+p users ["Erik"][:lottery_numbers].min
 # 6. Return an array of Avril's lottery numbers that are even
+result = []
+for number in users["Avril"][:lottery_numbers]
+  result << number if number.even?
+end
+p result
 # 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
+p users["Erik"][:lottery_numbers].push(7)
 # 8. Change Erik's hometown to Edinburgh
+p users["Erik"][:home_town] = "Edinburgh"
 # 9. Add a pet dog to Erik called "Fluffy"
+dog = {
+  :name => "Fluffy",
+  :species => "dog"
+}
+users["Erik"][:pets] << dog
 # 10. Add another person to the users hash
+
+Sandy = {
+  :twitter => "bridgpally",
+  :lottery_numbers => [12, 14, 33, 38, 9, 25],
+  :home_town => "Dunbar",
+  :pets => [
+    {
+      :name => "monty",
+      :species => "snake"
+    }
+  ]
+}
+
+users["Sandy M"] = Sandy
+
+p users
